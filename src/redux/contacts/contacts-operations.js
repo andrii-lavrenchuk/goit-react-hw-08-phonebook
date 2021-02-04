0002aa1567f4024f0a661784511e9b6af26d2c83
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-04 20:07:47 +0200

    Create contacts-operations.js

---
+++ src/redux/contacts/contacts-operations.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import {
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+  fetchContactRequest,
+  fetchContactSuccess,
+  fetchContactError,
+} from './contacts-actions';
+
+axios.defaults.baseURL = 'http://localhost:4040';
+
+const fetchContacts = () => async dispatch => {
+  dispatch(fetchContactRequest());
+
+  try {
+    const { data } = await axios.get('/contacts');
+
+    dispatch(fetchContactSuccess(data));
+  } catch (error) {
+    dispatch(fetchContactError(error));
+  }
+
+  //   axios
+  //     .get('/contacts')
+  //     .then(({ data }) => dispatch(fetchContactSuccess(data)))
+  //     .catch(error => dispatch(fetchContactError(error)));
+};
+
+const addContact = (name, number) => dispatch => {
+  const contact = {
+    name,
+    number,
+  };
+
+  dispatch(addContactRequest());
+
+  axios
+    .post('/contacts', contact)
+    .then(({ data }) => dispatch(addContactSuccess(data)))
+    .catch(error => dispatch(addContactError(error)));
+};
+
+const deleteContact = contactId => dispatch => {
+  dispatch(deleteContactRequest());
+
+  axios
+    .delete(`contacts/${contactId}`)
+    .then(() => dispatch(deleteContactSuccess(contactId)))
+    .catch(error => dispatch(deleteContactError(error)));
+};
+
+export default {
+  fetchContacts,
+  addContact,
+  deleteContact,
+};
