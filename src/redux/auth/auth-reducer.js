2230cc8d429886b8a2b2dd334f02e9d230ba3770
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-05 21:51:29 +0200

    Create auth-reducer.js

---
+++ src/redux/auth/auth-reducer.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+import authActions from './auth-actions';
+
+const initialUserState = { name: null, email: null };
+
+const user = createReducer(initialUserState, {
+  [authActions.registerSuccess]: (_, { payload }) => payload.user,
+});
+
+const token = createReducer(null, {
+  [authActions.registerSuccess]: (_, { payload }) => payload.token,
+});
+
+// const setError = (_, { payload }) => payload;
+
+const error = createReducer(null, {});
+
+export default combineReducers({
+  user,
+  token,
+  error,
+});
