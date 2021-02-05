f9135506c24caf6165d1f9aea2e69f90c426c26b
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-06 00:17:22 +0200

    Update UserMenu.js

--- src/Components/UserMenu/UserMenu.js
+++ src/Components/UserMenu/UserMenu.js
@@ -1,5 +1,5 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import { authSelectors, authOperations } from '../../redux/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authOperations } from '../../redux/auth';
 import defaultAvatar from './default-avatar.png';
 
 const styles = {
@@ -17,18 +17,15 @@ const styles = {
 };
 
 export default function UserMenu() {
-  //   const dispatch = useDispatch();
-  //   const name = useSelector(authSelectors.getUsername);
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUsername);
   const avatar = defaultAvatar;
 
   return (
     <div style={styles.container}>
       <img src={avatar} alt="" width="32" style={styles.avatar} />
-      <span style={styles.name}>Добро пожаловать, User</span>
-      <button
-        type="button"
-        //   onClick={() => dispatch(authOperations.logOut())}
-      >
+      <span style={styles.name}>Добро пожаловать, {name}</span>
+      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
         Выйти
       </button>
     </div>
