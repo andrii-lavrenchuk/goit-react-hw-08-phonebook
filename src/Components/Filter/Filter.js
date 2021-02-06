0d87fd6bc1afc6accbe4c42a9998753ef0812095
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-06 20:06:15 +0200

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -6,17 +6,23 @@ import s from './Filter.module.css';
 
 export default function Filter() {
   const value = useSelector(contactsSelectors.getFilter);
-
+  const contacts = useSelector(contactsSelectors.getContacts);
   const dispatch = useDispatch();
 
   return (
-    <label className={s.label}>
-      Find contacts by name
-      <input
-        type="text"
-        value={value}
-        onChange={e => dispatch(contactActions.changeFilter(e.target.value))}
-      ></input>
-    </label>
+    <>
+      {contacts.length > 0 && (
+        <label className={s.label}>
+          Find contacts by name
+          <input
+            type="text"
+            value={value}
+            onChange={e =>
+              dispatch(contactActions.changeFilter(e.target.value))
+            }
+          ></input>
+        </label>
+      )}
+    </>
   );
 }
